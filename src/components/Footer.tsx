85baf876cea520646350cd28c6377592b50ca97d
Saransh <hellosaranshh@gmail.com>
2023-08-11 21:44:14 +0530

    Bottom IOs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,8 +11,8 @@ const Footer: React.FC = () => {
   expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 0);
 
   return (
-    <div className="absolute z-30 bottom-4 w-full">
-      <div className="relative grid grid-cols-3 gap-4 text-white bg-black/40 p-4  mx-auto rounded-md backdrop-blur-sm w-full">
+    <div className="absolute z-30 bottom-20 sm:bottom-4 w-full">
+      <div className="relative grid grid-cols-3 gap-4 text-white bg-black/40 p-4  mx-auto rounded-md backdrop-blur-sm w-full h-full">
         <div className="flex items-center justify-start text-xl font-bold font-mono">
           {/* <span>Made by Phuc Le, inspired by Lofi.co</span> */}
           <span>{songs[currentSongIndex].name}</span>
